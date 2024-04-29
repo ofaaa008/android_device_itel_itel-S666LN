c35bbe0999e7bb3cfe50fdba0223ad6c8062b053
ofaaa008 <166842264+ofaaa008@users.noreply.github.com>
2024-04-29 23:46:21 +0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,27 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_itel-S666LN-user
-add_lunch_combo omni_itel-S666LN-userdebug
-add_lunch_combo omni_itel-S666LN-eng
+TFILE=$PWD/out/hapticspath.patched
+[ ! -d "out" ]&& mkdir -p out
+RET=0
+REVERSE=0
+
+cd bootable/recovery
+git apply --reverse --check ../../device/itel/S666LN/patches/0001-Change-haptics-activation-file-path.patch || REVERSE=$?
+cd ../../
+
+if [ -f "$TFILE" ];then
+    echo "haptics path patched already, skipping"
+elif [ $REVERSE -eq 0 ]; then
+    echo "$TFILE is not found but git is able to reverse haptics path patch, assuming it's already patched, skipping"
+else
+    cd bootable/recovery
+    git apply ../../device/itel/S666LN/patches/0001-Change-haptics-activation-file-path.patch || RET=$?
+    cd ../../
+    if [ $RET -ne 0 ];then
+        echo "ERROR: minuitwrp/events.cpp could not be patched! Vibration in TWRP will not work."
+    else
+        echo "OK: minuitwrp/events.cpp patched"
+        touch $TFILE
+    fi
+fi
